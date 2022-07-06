860c4c616f79dfef924846b00f6830bafdf14b01
Lucas Ramon <78308822+lramon2001@users.noreply.github.com>
2022-07-05 21:41:28 -0300

    Update Olamundo.s

--- somativa 1/Olamundo.s
+++ somativa 1/Olamundo.s
@@ -2,7 +2,10 @@
 	# Bloco para dados em memória
 	mensagem: .asciiz "Ola Mundo"  #Inidica com .asciiz que a variavel mensagem é uma string e atribui Ola mundo a esta variavel
 .text
+main:
 	# Bloco para escrita de comandos
 	li $v0,4 # comando para imprimir uma cadeia de caracteres 
 	la $a0, mensagem # move o conteudo de mensagem para o registrador $a0
 	syscall # comando para executar as instruçõ
+	li $v0, 10
+	syscall
